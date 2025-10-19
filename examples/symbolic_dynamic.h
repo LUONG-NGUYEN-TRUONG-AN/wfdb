2d192a7ed1ef0ca9856b06f712fbc99827726448
idonthavename <23C42002@student.hcmus.edu.vn>
2025-10-19 14:26:57 +0700

    Change data types in symbolic_dynamic_state_t

--- examples/symbolic_dynamic.h
+++ examples/symbolic_dynamic.h
@@ -4,7 +4,8 @@
 #include "project.h"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include "project.h" // For WFDB_Time
@@ -12,21 +13,21 @@ extern "C" {
 #define SYMBOLIC_X_DELAY 63
 #define SYMBOLIC_XL_DELAY 47
 
-// State structure to hold the history for the filter
-typedef struct {
-    WFDB_Time x_buffer[SYMBOLIC_X_DELAY];   // Circular buffer for x samples
-    int32_t xl_buffer[SYMBOLIC_XL_DELAY]; // Circular buffer for xl samples
-    int x_index;
-    int xl_index;
-} symbolic_dynamic_state_t;
+    // State structure to hold the history for the filter
+    typedef struct
+    {
+        uint16_t x_buffer[SYMBOLIC_X_DELAY];   // Circular buffer for x samples
+        uint16_t xl_buffer[SYMBOLIC_XL_DELAY]; // Circular buffer for xl samples
+        int x_index;
+        int xl_index;
+    } symbolic_dynamic_state_t;
 
-void symbolic_dynamic_init(symbolic_dynamic_state_t *state);
-uint8_t symbolic_dynamic_update(symbolic_dynamic_state_t *state, WFDB_Time x, int32_t xl, int32_t xh);
-
+    void symbolic_dynamic_init(symbolic_dynamic_state_t *state);
+    uint8_t symbolic_dynamic_update(symbolic_dynamic_state_t *state, uint16_t x,
+                                    uint16_t xl, uint16_t xh);
 
 #ifdef __cplusplus
 }
 #endif
-
 
 #endif /* SYMBOLIC_DYNAMIC_H */
