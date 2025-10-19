b526b8902b7cdac352d21dd5f3f3c04e3d765e2a
idonthavename <23C42002@student.hcmus.edu.vn>
2025-10-19 14:26:16 +0700

    Change WFDB_Time to uint16_t in median_filter

--- examples/median_filter.h
+++ examples/median_filter.h
@@ -1,7 +1,7 @@
 #ifndef MEDIAN_FILTER_H
 #define MEDIAN_FILTER_H
 
-#include "project.h"
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,19 +10,17 @@ extern "C" {
 #define MEDIAN_FILTER_SIZE 17
 
 typedef struct {
-    WFDB_Time window[MEDIAN_FILTER_SIZE]; // Circular buffer for incoming samples
-    WFDB_Time sorted[MEDIAN_FILTER_SIZE]; // A sorted copy of the window
-    int index;                            // Current position in the circular buffer
-    int count;                            // Number of samples in the window (for initialization)
+	uint16_t window[MEDIAN_FILTER_SIZE]; // Circular buffer for incoming samples
+	uint16_t sorted[MEDIAN_FILTER_SIZE]; // A sorted copy of the window
+	int index; // Current position in the circular buffer
+	int count; // Number of samples in the window (for initialization)
 } median_filter_t;
 
 void median_filter_init(median_filter_t *filt);
-int32_t median_filter_update(median_filter_t *filt, WFDB_Time new_sample);
-
+uint16_t median_filter_update(median_filter_t *filt, uint16_t new_sample);
 
 #ifdef __cplusplus
 }
 #endif
-
 
 #endif /* MEDIAN_FILTER_H */
